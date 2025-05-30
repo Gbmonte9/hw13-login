6bfda448964139c82039dc50c28c40fd3720e5c0
Gabriel Monte <gabrielmonte485@gmail.com>
2025-05-30 13:58:04 -0300

    Atualizei o Index para ter acesso com a criptografia

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../db');
+const { comparePasswords } = require('../bcrypt');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
@@ -11,18 +11,20 @@ router.post('/', async function(req, res, next) {
   const { vemail, vsenha } = req.body;
 
   try {
- 
+
     const user = await db.oneOrNone('SELECT * FROM usuarios WHERE email = $1', [vemail]);
 
     if (!user) {
       return res.render('index', { title: 'Express', error: 'Usuário não encontrado.' });
     }
 
-    if (user.senha !== vsenha) {
+    const acesso = await comparePasswords(vsenha, user.senha); 
+
+    if (!acesso) {
       return res.render('index', { title: 'Express', error: 'Senha incorreta.' });
     }
 
-    res.render('usuario', { title: 'Usuario', user });
+    res.render('usuario', { title: 'Usuário', user });
 
   } catch (err) {
     console.error(err);
